af978d11502929a130538ef961bc2be4e47102cb
Dennis Cherchenko <Dennis.Cherchenko@gmail.com>
2016-03-23 22:49:34 -0400

    Added website with Facebook login
    Added a simple website that lets user login with Facebook

---
+++ Website/js/login.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	
+
+
+
+});
+
+
+var employees = {"employees":[
+    {"firstName":"John", "lastName":"Doe"},
+    {"firstName":"Anna", "lastName":"Smith"},
+    {"firstName":"Peter", "lastName":"Jones"}
+]}
+
+
